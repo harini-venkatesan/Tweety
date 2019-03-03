6e0d74632cf8d91669d8f3723829e0c7de8f2a97
Harini Venkatesan <hvenk001@ucr.edu>
2019-03-03 14:34:20 -0800

    Create stream.py

---
+++ stream.py
@@ -0,0 +1,70 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json 
+import sys
+import collections
+import re
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+API_KEY = ''# add api_key from twitter account
+SECRET_KEY = ''# add secret_key from twitter account
+
+ACCESS_TOKEN = ''# add access token
+ACCESS_TOKEN_SECRET = ''
+
+fw = open('CS172_data.json', 'a')
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_status(self, status):
+
+    
+        
+        if(status.entities.get('hashtags')) == []:
+            return
+
+    
+        else:
+            if(not status.retweeted) and ('RT @' not in status.text):
+                parsed_tweet = {'user_name': status.user.screen_name}
+                parsed_tweet.update({'tweet_text': status.text})
+
+                entities = status.entities
+                hash_tags = []
+                for item in entities.get('hashtags'):
+                    hash_tags.append(item.get('text'))
+                parsed_tweet.update({'hash_tags': hash_tags})
+
+                #parsed_tweet.update({'created_at': status.created_at})
+                parsed_tweet.update({'tweet_id': status.id})  
+                parsed_tweet.update({'retweet_count': status.retweet_count})
+                parsed_tweet.update({'favorite_count': status.favorite_count})                    
+                parsed_tweet.update({'Coordinates': status.coordinates})
+                #order_parsed_tweet = collections.OrderedDict(sorted(parsed_tweet.items()))
+                print(parsed_tweet)
+                fw.write(json.dumps(parsed_tweet))
+                fw.write("\n\n")
+
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+          
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(API_KEY, SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    stream = Stream(auth, l, tweet_mode = 'extended')
+    
+    stream.filter(languages=["en"], track=['the','a','an'])
+  #  stream.filter(languages=["en"], track=['the','a','an'])
+
